f2dcdd981a95b79072ebcdb786405b2b2e6e8d45
MarianaSDiaz <maruuus.95@gmail.com>
2021-05-11 13:23:21 -0300

    BQA-14: page.js url changed

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -1,6 +1,5 @@
 module.exports = class page {
-
     open (path) {
-        return browser.url(`http://localhost:4000/${path}`)
+        return browser.url(`https://marianasdiaz.github.io/Task11/${path}`)
     }
 }
