4143f6bbb6462ee54dae54ca5a3975d2f3e7cd1a
MarianaSDiaz <maruuus.95@gmail.com>
2021-05-15 14:06:00 -0300

    BQA-14: register corrected

--- test/pageobjects/register.page.js
+++ test/pageobjects/register.page.js
@@ -1,15 +1,50 @@
-  
-const page = require ('../pageobjects/page')
-class RegisterPage extends page {
-
-    get inputUsername () { return $('#name') }
+class RegisterPage {
+    get inputName () { return $('#fname') }
+    get nameError () { return $('#fnameError') }
     get inputEmail () { return $('#email') }
-    get inputPassword () { return $('#password') }
-    get inputConfirmPass () { return $ ('#confirm') }
-    get btnSubmit () { return $('button[type="submit"]') }
+    get emailError () { return $('#emailError') }
+    get inputPassword () { return $('#passw') }
+    get passwordError () { return $('#passwError') }
+    get inputConfirm () { return $('#rpassw') }
+    get confirmError () { return $('#rpasswError') }
+    get btnSubmit () { return $$('.submit')[0] }
+    get btnReset () { return $$('.reset')[0] }
+    get validations () { return $('#reg-alert') }
 
+    completeRegister (name, email, password, confirm) {
+        this.inputName.setValue(name);
+        this.inputEmail.setValue(email);
+        this.inputPassword.setValue(password);
+        this.inputConfirm.setValue(confirm);
+    }
+    setName (value){
+        this.inputName.click();
+        this.inputName.setValue(value);
+        this.inputName.keys("Tab");
+    }
+    setEmail (value){
+        this.inputEmail.click();
+        this.inputEmail.setValue(value);
+        this.inputEmail.keys("Tab");
+    }
+    setPassword (value){
+        this.inputPassword.click();
+        this.inputPassword.setValue(value);
+        this.inputPassword.keys("Tab");
+    }
+    setConfirm (value){
+        this.inputConfirm.click();
+        this.inputConfirm.setValue(value);
+        this.inputConfirm.keys("Tab");
+    }
+    allSets(){
+        this.setName();
+        this.setEmail();
+        this.setPassword();
+        this.setConfirm();
+    };
     open () {
-        return super.open('register');
+        return browser.url('https://marianasdiaz.github.io/Task9/register.html');
     }
 }
 
--- test/specs/register.js
+++ test/specs/register.js
@@ -1,11 +1,77 @@
-const RegisterPage = require ('../pageobjects/register.page');
+const RegisterPage = require("../pageobjects/register.page");
 
-describe('Register form', () => {
-    it('should deny access with wrong username', () => {
-        RegisterPage.open ();
-        RegisterPage.email.setValue('maru@gmail.com')
-        RegisterPage.password.setValue('Maru123')
-        RegisterPage.submit()
-        expect(RegisterPage.alertEmail).toHaveText("Invalid email address")
-    })
-});
+describe("Register automation", () =>{
+    beforeAll("Open browser", () => {
+        RegisterPage.open();
+    });
+    describe("Register name testing", () => {
+        it("if name input is empty show the proper msg", () => {
+            RegisterPage.inputName.click();
+            RegisterPage.inputName.keys("Tab");
+            expect(RegisterPage.nameError).toHaveText("The name must contain at least 6 letters and a space");
+            browser.pause(3000);    
+        });
+        it("if name is invalid show the proper msg", () => {
+            RegisterPage.setName("maru");
+            expect(RegisterPage.nameError).toHaveText("The name must contain at least 6 letters and a space");
+            browser.pause(2000);    
+        });
+    });
+    describe("Register email testing", () => {
+        it("if email input is empty show the proper msg", () => {
+            RegisterPage.inputEmail.click();
+            RegisterPage.inputEmail.keys("Tab");
+            expect(RegisterPage.emailError).toHaveText("The email must contain a valid format");
+            browser.pause(3000);    
+        });
+        it("if email is invalid show the proper msg", () => {
+            RegisterPage.setEmail("maru.com");
+            expect(RegisterPage.emailError).toHaveText("The email must contain a valid format");
+            browser.pause(2000);    
+        });
+    });
+    describe("Register password testing", () => {
+        it("if password input is empty show the proper msg", () => {
+            RegisterPage.inputPassword.click();
+            RegisterPage.inputPassword.keys("Tab");
+            expect(RegisterPage.passwordError).toHaveText("The password must contain at least 8 characters, consisting of letters and numbers");
+            browser.pause(3000);    
+        });
+        it("if password is invalid show the proper msg", () => {
+            RegisterPage.setPassword("invalidpassword");
+            expect(RegisterPage.passwordError).toHaveText("The password must contain at least 8 characters, consisting of letters and numbers");
+            browser.pause(2000);    
+        });
+    });
+    describe("Register confirm password testing", () => {
+        it("if confirm input is empty show the proper msg", () => {
+            RegisterPage.inputConfirm.click();
+            RegisterPage.inputConfirm.keys("Tab");
+            expect(RegisterPage.confirmError).toHaveText("Passwords do not match");
+            browser.pause(3000);    
+        });
+        it("if confirm password is wrong show the proper msg", () => {
+            RegisterPage.setConfirm("wrongpass01");
+            expect(RegisterPage.confirmError).toHaveText("Passwords do not match");
+            browser.pause(2000);    
+        });
+    });
+    describe("Testing Reset btn", () => {
+        it("Reset btn erase all inputs", () => {
+            RegisterPage.btnSubmit.keys("Tab");
+            RegisterPage.btnReset.keys("Enter");
+            expect(RegisterPage.allSets()).toEqual();
+            browser.pause(3000);  
+        });
+    });
+    describe("Testing Register btn", () => {
+        it("Register btn display user data", () => {
+            RegisterPage.completeRegister("Valid User", "validemail@gmail.com", "password01", "password01");
+            RegisterPage.btnSubmit.keys("Tab");
+            RegisterPage.btnSubmit.keys("Enter");
+            expect(RegisterPage.validations).toHaveTextContaining(["Valid User", "validemail@gmail.com",
+            "password01", "password01"]);
+            browser.pause(3000);  
+        });
+    });
+})
